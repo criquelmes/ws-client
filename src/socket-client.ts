c8138829978e0ebfab9ee8d0fff5d0da8716aa09
Christopher Riquelme <ccriquelmes@gmail.com>
2024-12-31 01:56:45 -0300

    Sección 15 Curso de Nest: Cliente - Vite Vanilla Typescript

---
+++ src/socket-client.ts
@@ -0,0 +1,8 @@
+import { Manager } from "socket.io-client";
+
+export const connectToSocket = () => {
+  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
+
+  const socket = manager.socket("/");
+  console.log({ socket });
+};
