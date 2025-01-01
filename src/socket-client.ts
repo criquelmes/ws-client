230282a9ed1555b6bfd42dbf17a51e501dce6d24
Christopher Riquelme <ccriquelmes@gmail.com>
2025-01-01 19:03:31 -0300

    Sección 15: Cliente - Detectar conexión y desconexón

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,8 +1,20 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToSocket = () => {
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
 
   const socket = manager.socket("/");
-  console.log({ socket });
+  addListener(socket);
+};
+
+const addListener = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector("#server-status");
+
+  socket.on("connect", () => {
+    serverStatusLabel!.innerHTML = "online";
+  });
+
+  socket.on("disconnect", () => {
+    serverStatusLabel!.innerHTML = "offline";
+  });
 };
