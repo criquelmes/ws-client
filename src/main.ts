57e70e1ffb896d0b01cb174b90365e73fa797360
Christopher Riquelme <ccriquelmes@gmail.com>
2025-01-14 21:48:39 -0300

    Sección 15: Preparar cliente para enviar JWT

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import "./style.css";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <h1>Websocket - client</h1>
+    <h2>Websocket - client</h2>
+    <input id="jwtToken" placeholder="JWT Token">
+    <button id="btn-connect">Connect</button>
+
+    <br />
     <span id="server-status">offline</span>
     <ul id="clients-ul">
     </ul>
@@ -16,4 +20,14 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
-connectToSocket();
+// connectToSocket();
+
+const btnConnect = document.querySelector("#btn-connect")! as HTMLButtonElement;
+const jwtToken = document.querySelector<HTMLInputElement>("#jwtToken")!;
+
+btnConnect.addEventListener("click", () => {
+  if (jwtToken.value.trim().length <= 0)
+    return alert("Please enter a JWT token");
+
+  connectToSocket(jwtToken.value.trim());
+});
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,7 +1,12 @@
 import { Manager, Socket } from "socket.io-client";
 
-export const connectToSocket = () => {
-  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
+export const connectToSocket = (token: string) => {
+  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js", {
+    extraHeaders: {
+      hola: "mundo",
+      authentication: token,
+    },
+  });
 
   const socket = manager.socket("/");
   addListener(socket);
